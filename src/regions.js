dac83e1bc14c7601ee1aff451a6c5a1805286f48
Allie Tran <ly.tran2@mail.dcu.ie>
2023-01-17 18:42:41 +0000

    fixed bugs

---
+++ src/regions.js
@@ -0,0 +1,3 @@
+var regions=["dublin", "greece", "the municipal district of naas", "leinster", "europe", "kildare", "thessaloniki", "thessaloniki regional unit", "ireland", "the municipal district of kildare -- newbridge", "macedonia and thrace", "thessaloniki municipal unit"];
+
+export default regions;
